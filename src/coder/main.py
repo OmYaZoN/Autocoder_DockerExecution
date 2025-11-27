6b77fb254d20de301e5663b3af66b30ffff86dc9
OmYaZoN <om.arun.raut@gmail.com>
2025-11-27 20:46:38 +0530

    added full project code

---
+++ src/coder/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+import warnings
+import os
+from datetime import datetime
+
+from coder.crew import Coder
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# Create output directory if it doesn't exist
+os.makedirs('output', exist_ok=True)
+
+assignment = 'Write a python program using recursion to calculate the total number \
+of ways to climb a staircase of 8 steps, where a person can take 1, 2, or 3 steps \
+at a time. Return only the number of ways.'
+
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'assignment': assignment,
+    }
+    
+    result = Coder().crew().kickoff(inputs=inputs)
+    print(result.raw)
+
+
+
+
